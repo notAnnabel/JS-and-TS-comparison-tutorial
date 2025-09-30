83894144aa6711ee91a4ee9d372a3b432b298779
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-09-30 15:36:43 +0100

    9 exercises

---
+++ exercise6.js
@@ -0,0 +1,13 @@
+//SUBTRACTING WITH FUNCTION
+
+// 1. Create subtraction function that takes two parameters and adds them
+// This function should return the subtraction
+function subtract(x, y){
+  return x - y;
+}
+
+// 2. Execute the function with two example numbers and store the result in a variable
+const result = subtract(10, 2);
+
+// 3. Log the result
+console.log(result);
---
+++ exercise7.js
@@ -0,0 +1,15 @@
+// ARROW FUNCTION
+
+// 1. Create a function that logs Hello World
+function helloWorldTraditional() {
+  console.log("Hello World");
+}
+
+// 2. Create the equivalent Arrow Function
+const helloWorldArrow = () => {
+  console.log("Hello World");
+}
+
+// 3. Execute both functions
+helloWorldTraditional();
+helloWorldArrow();
