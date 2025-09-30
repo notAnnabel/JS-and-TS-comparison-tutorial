f2a0e408d84836d6c3ebaac0f331a080a76b1c21
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-09-30 15:43:00 +0100

    ex 10 - objects

---
+++ exercise10.js
@@ -0,0 +1,19 @@
+// OBJECTS
+
+// 1. Create a constant person object with name 'Mary' and age 25
+const person = {
+  name: "Mary",
+  age: 25
+}
+
+// 2. Log the person's name
+console.log(person.name);
+
+// 3. Log the person's age
+console.log(person.age);
+
+// 4. Change the person's age to another number
+person.age = 30;
+
+// 5. Log the new age
+console.log(person.age);
