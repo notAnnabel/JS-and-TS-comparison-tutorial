253923dc531e849d362977bc09f5ec33feec4d38
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2025-10-07 13:52:25 +0100

    yay :D

--- ts/exercise14.ts
+++ ts/exercise14.ts
@@ -1,14 +1,16 @@
-// TYPING FUNCTIONS
+// INTERFACES
 
-// This is a possible type for person:
-type Person = {
-    name: string,
-    age: number,
-    gender: "F"|"M"|"NB"|"other";
-};
+// Interfaces are a simplification of types
+// Interfaces are used when a type is needed for one file, while types can be accessed from all project
+interface Person {
+  name: string;
+  age: number;
+  gender: "F"|"M"|"NB"|"other";
+}
 
 // 1. Create a constant person object with name 'Mary' and age 25
 const human : Person={name:"Mary", age:25, gender:"F"};
+
 // 2. Create a function that logs a the person's name and age - using the Person as type
 function logging(person: Person){
     console.log(person.name)
@@ -17,4 +19,4 @@ function logging(person: Person){
 }
 // 3. Use the function to log Mary's details
 logging(human);
-// 4. Modify the above code to add gender to the type person with a list of possible genders and fix all errors
+// 4. Modify the above code to add gender to the interface person with a list of possible genders and fix all errors
